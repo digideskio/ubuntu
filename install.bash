6501d80e24be94cc84a0e536bc23eb242dfda60d
Jordon Bedwell <jordon@envygeeks.io>
2016-05-27 00:51:10 -0500

    Delete install.bash

--- install.bash
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-echo "Copying etc to /etc" && cd etc && cp -R ./* /etc && cd -
-printf "Copying home to %s" $HOME && cd home && cp -R ./{*,.*} $HOME && cd -
-echo "Copying usr to /usr" && cd usr && cp -R ./* /usr && cd -
-echo "Done!"
